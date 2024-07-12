5dea0a2a96161306eb9e0c44ced2ffe27d17a071
Alwiya Pratiwi <alwiyapratiwi00@gmail.com>
2024-07-12 20:56:19 +0700

    update

---
+++ src/test/java/alwiya/carRent/BelajarCarRentApplicationTests.java
@@ -0,0 +1,13 @@
+package alwiya.carRent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarCarRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
